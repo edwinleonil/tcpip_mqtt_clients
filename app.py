dbdccca92e761945de09e0086e573d782060edbe
E L Anarcaya Roca <E.Anarcaya-Roca@sheffield.ac.uk>
2023-09-05 15:56:48 +0100

    app.py refaactored with pyqt6

--- app.py
+++ app.py
@@ -1,57 +1,100 @@
-import tkinter as tk
-import yaml
 import socket
+import sys
 import time
+import yaml
 import csv
 import os
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QLineEdit, QPushButton, QTextEdit
+from PyQt6.QtCore import QThread, pyqtSignal, QObject
 
-
-class App:
-    def __init__(self, master):
-        self.master = master
-        master.title("Client App")
-        # set the window size
-        master.geometry("600x400")
+class App(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Client App")
+        self.setGeometry(100, 100, 600, 400)
         self.config_file_path = "config.yaml"
         self.client_socket = None
         self.connected = False
-        self.stop_logging = False
         self.logging = False
-        
+
         with open("config.yaml", "r") as f:
             config = yaml.safe_load(f)
 
         self.host = config["ip_address"]
         self.port = config["port_number"]
+   
+        self.host = str(self.host)
+        self.port = int(self.port)
 
-        self.ip_label = tk.Label(master, text="IP Address:")
-        self.ip_label.grid(row=0, column=0, padx=10, pady=10)
+        self.csv_file_path = "data/"
 
-        self.ip_var = tk.StringVar(value=config["ip_address"])
-        self.ip_entry = tk.Entry(master, textvariable=self.ip_var)
-        self.ip_entry.grid(row=0, column=1, padx=10, pady=10)
+        self.central_widget = QWidget()
+        self.setCentralWidget(self.central_widget)
 
-        self.port_label = tk.Label(master, text="Port Number:")
-        self.port_label.grid(row=1, column=0, padx=10, pady=10)
+        self.ip_label = QLabel("IP Address:")
+        self.ip_var = QLineEdit(str(self.host))
+        self.port_label = QLabel("Port Number:")
+        self.port_var = QLineEdit(str(self.port))
+        self.connect_button = QPushButton("Connect")
+        self.start_button = QPushButton("Start Client")
+        self.stop_button = QPushButton("Stop Client")
+        self.stop_button.setEnabled(False)
+        self.status_label = QLabel("Status:")
+        self.status_text = QTextEdit()
+        self.status_text.setReadOnly(True)
 
-        self.port_var = tk.StringVar(value=config["port_number"])
-        self.port_entry = tk.Entry(master, textvariable=self.port_var)
-        self.port_entry.grid(row=1, column=1, padx=10, pady=10)
+       
+        layout = QVBoxLayout()
+        layout.addWidget(self.ip_label)
+        layout.addWidget(self.ip_var)
+        layout.addWidget(self.port_label)
+        layout.addWidget(self.port_var)
+        layout.addWidget(self.connect_button)
+        layout.addWidget(self.start_button)
+        layout.addWidget(self.stop_button)
+        layout.addWidget(self.status_label)
+        layout.addWidget(self.status_text)
+        
 
-        self.start_button = tk.Button(master, text="Start Client", command=self.start_client)
-        self.start_button.grid(row=2, column=0, padx=10, pady=10)
+        self.central_widget.setLayout(layout)
 
-        self.stop_button = tk.Button(master, text="Stop Client", command=self.stop_client, state=tk.DISABLED)
-        self.stop_button.grid(row=2, column=1, padx=10, pady=10)
+        self.start_button.clicked.connect(self.logging_data)
+        self.stop_button.clicked.connect(self.stop_client)
+        self.connect_button.clicked.connect(self.connect_to_server)
+        
+        self.update_config()
 
-        self.status_label = tk.Label(master, text="Status:")
-        self.status_label.grid(row=3, column=0, padx=10, pady=10)
+    def connect_to_server(self):
 
-        self.status_text = tk.Text(master, height=10, width=50)
-        self.status_text.grid(row=3, column=1, padx=10, pady=10)
+        # Create a new thread to run connect_tcpip function
+        self.tcpip_thread = QThread()
+        # Create a new worker to run connect_tcpip function
+        self.tcpip_worker = TcpipWorker(self.host, self.port, self)
+        # Move the worker to the thread
+        self.tcpip_worker.moveToThread(self.tcpip_thread)
+        # Connect the worker's finished signal to the thread's quit method
+        self.tcpip_worker.finished.connect(self.tcpip_thread.quit)
+        # Connect the thread's started signal to the worker's connect_tcpip method
+        self.tcpip_thread.started.connect(self.tcpip_worker.connect_tcpip)
+        # Start the thread
+        self.tcpip_thread.start()
 
-    def connection_setup(self):
+    def logging_data(self):
+        #start logging data thread and quit the thread when the logging is done
+        self.log_thread = QThread()
+        # Create a new worker to run connect_tcpip function
+        self.log_worker = LogThread(self.csv_file_path, self.tcpip_worker.connected, self.tcpip_worker.client_socket, self)
+        # Move the worker to the thread
+        self.log_worker.moveToThread(self.log_thread)
+        # Connect the worker's finished signal to the thread's quit method
+        self.log_worker.finished.connect(self.log_thread.quit)
+        # Connect the thread's started signal to the worker's connect_tcpip method
+        self.log_thread.started.connect(self.log_worker.log_data)
+        # Start the thread
+        self.log_thread.start()
 
+
+    def update_config(self):
         # get the IP address and port number from the config.yaml file
         with open(self.config_file_path) as f:
             config = yaml.safe_load(f)
@@ -69,156 +112,139 @@ class App:
         else:
             # add a csv file to the csv_file_path directory
             self.csv_file_path = self.csv_file_path + "data1.csv"
-        
-        # return self.host, self.port, self.csv_file_path
+        self.status_text.append("Config file updated\n")
+
 
     def start_client(self):
         self.client_socket = None
         self.connected = False
-        self.stop_logging = False
         self.logging = False
-
-        self.ip_address = self.ip_entry.get() or self.ip_var
-        self.port_number = self.port_entry.get() or self.port_var 
-        self.start_button.config(state=tk.DISABLED)
-        self.stop_button.config(state=tk.NORMAL)
-        
-        self.status_text.delete('1.0', tk.END)
-
-        self.status_text.insert(tk.END, "Client started\n")
-        self.status_text.update_idletasks()  # update widget immediately
-        self.status_text.after(1, self.update_status)
-        
-        self.update_config()
-        self.connection_setup()
-        
-        connected =  self.connect_tcpip()
-        
-        if connected:
-            self.log_data()
-        else:
-            self.status_text.insert(tk.END, "Connection failed\n")
-            self.status_text.update_idletasks()
+        # start the log thread
+        self.log_thread = LogThread()
+        self.log_thread.start()
 
 
-    # function to setup the TCP/IP connection 
+    def stop_client(self):
+        self.logging = False
+        self.stop_button.setEnabled(False)
+        self.start_button.setEnabled(True)
+        self.status_text.append("Client stopped\n")
+
+
+
+class TcpipWorker(QObject):
+    finished = pyqtSignal()
+
+    def __init__(self, host, port, App):
+        super().__init__()
+        self.host = str(host)
+        self.port = int(port)
+        self.app = App
+        self.connected = False
+        self.client_socket = None
+
+        self.app.start_button.setEnabled(False)
+        self.app.stop_button.setEnabled(True)
+        self.app.status_text.clear()
+        self.app.status_text.append("Client started\n")
+
     def connect_tcpip(self):
-
-        while not self.connected:
+        counter = 0
+        while True:
             try:
                 # create a TCP/IP socket
                 self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 # connect the socket to the server's address and port
                 self.client_socket.connect((self.host, self.port))
-                # check the status of the connection
-                self.client_socket.getsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE)
+                self.app.connect_button.setEnabled(False)
+                self.app.start_button.setEnabled(True)
+                self.app.stop_button.setEnabled(True)
                 self.connected = True
-                self.status_text.delete('1.0', tk.END)
-                self.status_text.insert(tk.END, "TCP/IP Connection is open.\n")
-                self.status_text.update_idletasks()  # update widget immediately
-                # self.status_text.after(1, self.update_status)
+                print("Connected to server.")
+                # if connection is successful, break the loop and end the thread
+                break              
 
             except ConnectionRefusedError:
-                self.connected = False
-                # print("Connection refused. Retrying in 1 second...")
-                self.status_text.insert(tk.END, "Connection refused. Retrying in 1 second...\n")
                 print("Connection refused. Retrying in 1 second...")
-                self.status_text.update_idletasks()  # update widget immediately
-                # self.status_text.after(1, self.update_status)     
-                time.sleep(1)           
-                
-            # exit the loop if the stop button is pressed
-            if self.stop_button["state"] == tk.DISABLED:
+                time.sleep(1)
+            counter += 1
+            if counter == 3:
+                print("Connection timed out.")
+                self.app.connect_button.setEnabled(True)
+                self.app.start_button.setEnabled(False)
+                self.app.stop_button.setEnabled(False)
                 break
-          
-        return self.connected
+        self.finished.emit()
+
+
+class LogThread(QThread):
+    finished = pyqtSignal()
+
+    def __init__(self, csv_file_path, connected, client_socket, App):
+        super().__init__()
+        self.app = App
+        self.csv_file_path = csv_file_path
+        self.connected = connected
+        self.client_socket = client_socket
+
+        # disable all the buttons except the stop button
+        self.app.start_button.setEnabled(False)
+        self.app.connect_button.setEnabled(False)
+        self.app.stop_button.setEnabled(True)
+
+        # update the status text
+        self.app.status_text.append("Logging ...\n")
 
     def log_data(self):
-        
-        # client_socket = self.client_socket
-        self.csv_file_path = self.csv_file_path
-        self.logging = self.connected 
 
         with open(self.csv_file_path, mode='w') as file:
             writer = csv.writer(file)
-            while self.logging:
-                
-                # send data to the server
-                message = "get data"
-                self.client_socket.sendall(message.encode('utf-8'))
 
-                # receive data from the server
-                data = self.client_socket.recv(1024)
+            while True:
 
-                if not data:
-                    # server has closed the connection, break out of the loop
-                    break
+                if self.connected == True:
+                    # send data to the server
+                    message = "get data"
+                    self.client_socket.sendall(message.encode('utf-8'))
 
-                if data.decode('utf-8') == "ON":
-                        self.status_text.insert(tk.END, "Logging data ...\n")
-                        self.status_text.update_idletasks()  # update widget immediately
-                        self.status_text.after(1, self.update_status)
-                        while True:
+                    # receive data from the server
+                    data = self.client_socket.recv(1024)
 
-                            # send data to the server
-                            message = "received"
-                            self.client_socket.sendall(message.encode('utf-8'))
+                    if not data:
+                        # server has closed the connection, break out of the loop
+                        break
 
-                            # receive data from the server
-                            data = self.client_socket.recv(1024)
+                    if data.decode('utf-8') == "ON":
 
-                            if not data:
-                                # server has closed the connection, break out of the loop
-                                break
+                            while True:
 
-                            if data.decode('utf-8') == "STOP":
-                                self.logging == False
-                                self.stop_logging = True
-                                self.status_text.insert(tk.END, "Logging data completed\n")
-                                self.status_text.update_idletasks()  # update widget immediately
-                                self.status_text.after(1, self.update_status)
-                                break
-                            else:
-                                writer.writerow([data.decode('utf-8')])  # write data to CSV file              
+                                # send data to the server
+                                message = "received"
+                                self.client_socket.sendall(message.encode('utf-8'))
+
+                                # receive data from the server
+                                data = self.client_socket.recv(1024)
+
+                                if not data:
+                                    # server has closed the connection, break out of the loop
+                                    break
+
+                                if data.decode('utf-8') == "STOP":
+
+                                    self.app.status_text.append("Logging stopped\n")
+                                    self.app.stop_button.setEnabled(True)
+                                    self.app.start_button.setEnabled(True)
+                                    self.app.connect_button.setEnabled(True)
+                                    break
+                                else:
+                                    writer.writerow([data.decode('utf-8')])  # write data to CSV file              
+        self.finished.emit()
         # close the csv file
         file.close()
-        return
 
-    def update_status(self):
-        # Update the widget and schedule another update
-        self.status_text.update_idletasks()
-        self.status_text.after(1, self.update_status)
 
-    def update_config(self):
-
-        with open("config.yaml",'r') as f:
-            config = yaml.safe_load(f)
-
-        config["ip_address"] = str(self.ip_address)
-        config["port_number"] = int(self.port_number)
-
-        with open("config.yaml",'w') as f:
-            yaml.dump(config, f)
-
-    def stop_client(self):
-        
-        # self.process.terminate()
-        self.start_button.config(state=tk.NORMAL)
-        self.stop_button.config(state=tk.DISABLED)
-        self.status_text.insert(tk.END, "Client stopped\n")
-        self.status_text.update_idletasks()  # update widget immediately
-        self.status_text.after(1, self.update_status)
-        
-        if self.stop_logging:
-            print("Connection was closed by the server.")
-        # check if the client is connected to the server
-        if self.connected:
-            # close the socket
-            self.client_socket.close()
-            print("Connection to server closed by client.")
-
-        
-
-root = tk.Tk()
-app = App(root)
-root.mainloop()
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = App()
+    window.show()
+    sys.exit(app.exec())    
